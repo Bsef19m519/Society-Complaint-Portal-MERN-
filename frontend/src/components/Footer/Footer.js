0ab592d5c0b896bc5119b1785d46b11d1c5e1065
razabutt863 <arbutt863@gmail.com>
2023-04-17 05:32:22 -0700

    footer set

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Footer.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+// import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; Society Complaint Portal</p>
-      <nav>
+      <p className="footerPara">&copy; Society Complaint Portal</p>
+      {/* <nav>
         <ul>
           <li><Link to="/" className='linkStyle'>Home</Link></li>
           <li><Link to="/about" className='linkStyle'>About</Link></li>
           <li><Link to="/service" className='linkStyle'>Services</Link></li>
         </ul>
-      </nav>
+      </nav> */}
     </footer>
   );
 }
