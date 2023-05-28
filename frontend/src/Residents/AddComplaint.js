6ac6db10b7dc7a0b8c4c5e0b7b97cd97c838c20a
Your Name <you@example.com>
2023-05-28 13:31:41 +0500

    residence moule design done.

--- frontend/src/Residents/AddComplaint.js
+++ frontend/src/Residents/AddComplaint.js
@@ -1,60 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import ScreenBtn from "../components/Button/ScreenButton";
-import "./AddComplaint.css";
 import { useNavigate } from "react-router-dom";
+import "./AddComplaint.css";
 
-const AddNewComplaint = () => {
-  let navigate = useNavigate();
+const AddComplaint = () => {
+  const navigate = useNavigate();
+  const [complaintType, setComplaintType] = useState("");
+  const [description, setDescription] = useState("");
+
   const goBack = () => {
-    navigate("/Resident-front-page");
+    navigate("/front-page");
   };
 
-  //creating alert for complainer
   const alertMessage = () => {
-    alert("Fake complaint will lead to penalty against complainer!!!");
+    alert("Fake complaints will lead to a penalty against the complainer!!!");
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Perform further actions with the complaintType and description values
+  };
+
+  const handleComplaintTypeChange = (event) => {
+    setComplaintType(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
   };
 
   return (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <h1 className="register-txt">Register Your Complaint Carefully!!!</h1>
-        <div className="Complaint-form-container">
-          <div className="register-container">
-            <label>Name:</label>
-            <br />
-            <input type="text" name="uname" required autoFocus />
-          </div>
-          <div className="register-container">
-            <label>Address:</label>
-            <br />
-            <input type="text" name="residance" required />
-          </div>
-          <div className="register-container">
-            <label>Description:</label>
-            <br />
-            <textarea name="w3review" rows="5" cols="100" />
-          </div>
-          <div className="register-container">
-            <label>Proof:</label>
-            <br />
-            <input type="file" name="G-proof" required />
-          </div>
-          <div className="complaint-button">
-            <ScreenBtn type="submit" onClick={alertMessage}>
-              Add Complaint
-            </ScreenBtn>
-            <ScreenBtn type="button" onClick={goBack}>
-              Back
-            </ScreenBtn>
-          </div>
+    <div className="AddComplaint-container-div">
+      <form className="AddComplaint-form-container" onSubmit={handleSubmit}>
+        <h1 className="AddComplaint-heading">Add Complaint</h1>
+        <div className="AddComplaint-input-container">
+          <label>Complaint Type:</label>
+          <select
+            value={complaintType}
+            onChange={handleComplaintTypeChange}
+            required
+          >
+            <option value="">Select an option</option>
+            <option value="Maintenance issue">Maintenance issue</option>
+            <option value="Security concerns">Security concerns</option>
+            <option value="Common area problems">Common area problems</option>
+            <option value="Noise and nuisance">Noise and nuisance</option>
+            <option value="Parking related issues">Parking related issues</option>
+            <option value="Complaint against any member">
+              Complaint against any member
+            </option>
+            <option value="Others">Others</option>
+          </select>
+        </div>
+        <div className="AddComplaint-input-container">
+          <label>Description:</label>
+          <textarea
+            name="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            rows="5"
+            minLength={10}
+            maxLength={1000}
+            required
+          />
+        </div>
+        <div className="AddComplaint-singleButton-container">
+          <ScreenBtn type="submit" onClick={alertMessage}>
+            Add 
+          </ScreenBtn>
+          <ScreenBtn type="button" onClick={goBack}>
+            Back
+          </ScreenBtn>
         </div>
       </form>
     </div>
   );
 };
 
-export default AddNewComplaint;
+export default AddComplaint;
--- frontend/src/Residents/ViewComplaint.js
+++ frontend/src/Residents/ViewComplaint.js
@@ -1,57 +1,67 @@
-import React from "react";
-import ScreenBtn from "../components/Button/ScreenButton";
-import "./ViewComplaint.css";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import './ViewComplaint.css';
+import ScreenButton from '../components/Button/ScreenButton';
+import { useNavigate } from 'react-router-dom';
+import getHeader from '../utils';
 
 const ViewComplaint = () => {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
   const goBack = () => {
-    navigate("/Resident-front-page");
+    navigate('/front-page');
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+  const [tableData, setTableData] = useState([]);
+//fectcomplaints is not the real function. the name will be modified with respect to the name in backend.
+  useEffect(() => {
+    const fetchComplaints = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/complaints', {
+          headers: { ...getHeader().get('Authorization') },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setTableData(data);
+        } else {
+          console.log('Error: Fetching complaints failed.');
+        }
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    };
+
+    fetchComplaints();
+  }, []);
 
   return (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <h1 className="view-txt">Check Your Complaint Status!!!</h1>
-        <div className="view-Complaint-form-container">
-          <div className="view-container">
-            <label>Address:</label>
-            <br />
-            <input
-              type="text"
-              name="usearch"
-              placeholder="search"
-              required
-              autoFocus
-            />
-          </div>
-          {/* <div className="view-container">
-          <label>Complaint Id:</label><br/>
-          <input type="text" name="residance"  required /> 
+    <div className="VC-Viewcomplaint-container-div">
+      <div className="VC-viewComplaint-form-container">
+        <h2 className="VC-viewComplaint-heading">View Complaint Status</h2>
+        <div className="VC-viewComplaint-singlebutton-container">
+          <ScreenButton type="button" onClick={goBack}>
+            Back
+          </ScreenButton>
         </div>
-        <div className="register-container">
-          <label>Description:</label><br/>
-          <textarea  name="w3review" rows="5" cols="100" />
-          
-        </div>
-        <div className="register-container">
-          <label>Proof:</label><br/>
-          <input type="file" name="G-proof"  required/>
-          </div>
-           */}
-
-          <div className="view-complaint-button">
-            <ScreenBtn type="submit">View Complaint</ScreenBtn>
-            <ScreenBtn type="button" onClick={goBack}>
-              Back
-            </ScreenBtn>
-          </div>
-        </div>
-      </form>
+      </div>
+      <table className="data-table">
+        <thead>
+          <tr>
+            <th className="data-table-header">Complaint Type</th>
+            <th className="data-table-header">Description</th>
+            <th className="data-table-header">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((dataItem) => (
+            <tr key={dataItem._id}>
+              <td>{dataItem.complaintType}</td>
+              <td>{dataItem.description}</td>
+              <td>{dataItem.complaintStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
