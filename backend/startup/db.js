59ab68c3a041875cae7a4c4929023db48c79b81c
bsef19m519 <bsef19m519@pucit.edu.pk>
2023-06-07 12:19:03 +0500

    DB is connection is available for all ip addresses

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
  * The method connects server to the database.
  */
 module.exports = function() {
-  const db = "mongodb+srv://bsef19m519:25July2000@cluster0.l0ahg1e.mongodb.net/societyComplaintPortalDB";
+  const db = "mongodb+srv://dbUser:complaintPortal2023@cluster0.l0ahg1e.mongodb.net/societyComplaintPortalDB";
   mongoose.connect(db)
-    .then(() => console.log(`Connected to ${db}...`))
+    .then(() => console.log(`Connected to MongoDB...`))
     .catch((err)=> console.log(`Could not connect to MongoDB...`));
 }
